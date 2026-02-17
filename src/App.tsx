a8c8203e1b6a896b7985856049457eabcf318d24
David Y. Stephenson <david@davidystephenson.com>
2026-02-17 13:55:52 -0500

    Remove router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import { BrowserRouter, Link } from 'react-router-dom'
-import Router from './Router'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import { useState } from 'react'
+import { GlobalContext } from './globalContext'
+import GuestList from './GuestList'
+import Home from './Home'
 
-export default function App () {
+export default function App() {
   const [guests, setGuests] = useState(['dorothy', 'zelda', 'tallulah'])
+
+  const value = { guests, setGuests }
 
   return (
     <BrowserRouter>
-      <h1>Party App ({guests.length} guests) </h1>
-      <div>
-        <Link to="/">Home</Link> | <Link to="/guests">Guests</Link>
-      </div>
-      <Router guests={guests} setGuests={setGuests}/>
+      <GlobalContext value={value}>
+        <h1>Party App ({guests.length} guests) </h1>
+        <div>
+          <Link to="/">Home</Link> | <Link to="/guests">Guests</Link>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home guests={guests} setGuests={setGuests} />} />
+          <Route path="/guests" element={<GuestList guests={guests} />} />
+        </Routes>
+      </GlobalContext>
     </BrowserRouter>
   )
 }
